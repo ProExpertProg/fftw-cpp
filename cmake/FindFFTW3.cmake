22ae0a0addb507eca4091879b633bd3caeacac1e
Luka Govedic <luka.govedic@gmail.com>
2024-07-15 22:16:09 -0400

    Comments, linking fixed

--- cmake/FindFFTW3.cmake
+++ cmake/FindFFTW3.cmake
@@ -1,5 +1,22 @@
-# Use as hints, any number of them can be passed
-# IF library dir is not passed, it is inferred from the library path or the include dir
+# This script is used to find the FFTW3 library.
+#
+# Output variables:
+# - FFTW3_FOUND, whether the library was found
+# - FFTW3_INCLUDE_DIRS, where to find fftw3.h
+# - FFTW3_LIBRARIES_DIR, where to find the library
+# - FFTW3_LIBRARIES, the library (and dependencies) to link against
+# - FFTW3::fftw3, an imported target for the library.
+#   This target also contains the proper include directories and dependencies needed
+#
+# Hints:
+# - FFTW3_INCLUDE_DIR, where to find fftw3.h
+# - FFTW3_LIBRARY_DIR, where to find the library
+# - FFTW3_LIBRARY, the library to link against
+# Any number of hints can be passed. The remaining ones are inferred from the ones passed,
+# assuming the library is installed in a `lib` directory next to the `include` directory,
+# which contains the header file.
+
+# If library dir is not passed, it is inferred from the library path or the include dir
 if (FFTW3_INCLUDE_DIR OR FFTW3_LIBRARY_DIR OR FFTW3_LIBRARY)
     if (FFTW3_LIBRARY AND NOT FFTW3_LIBRARY_DIR)
         # try to find based on lib path
@@ -19,6 +36,7 @@ if (FFTW3_INCLUDE_DIR OR FFTW3_LIBRARY_DIR OR FFTW3_LIBRARY)
         find_library(FFTW3_LIBRARY NAMES fftw3 HINTS ${FFTW3_LIBRARY_DIR} NO_DEFAULT_PATH)
     endif ()
 else ()
+    # No hints passed, look in default locations
     find_path(FFTW3_INCLUDE_DIR fftw3.h PATH_SUFFIXES fftw3)
     find_library(FFTW3_LIBRARY NAMES fftw3)
     if (FFTW3_LIBRARY)
@@ -27,6 +45,7 @@ else ()
     endif ()
 endif ()
 
+# handles REQUIRED, QUIET, output etc.
 find_package_handle_standard_args(FFTW3 DEFAULT_MSG FFTW3_LIBRARY FFTW3_INCLUDE_DIR)
 
 if (FFTW3_FOUND)
@@ -35,15 +54,15 @@ if (FFTW3_FOUND)
     set(FFTW3_INCLUDE_DIRS ${FFTW3_INCLUDE_DIR})
     set(FFTW3_LIBRARIES ${FFTW3_LIBRARY})
 
-    if (UNIX)
-        # Needs `m` on UNIX
-        set(FFTW3_LIBRARIES ${FFTW3_LIBRARIES} m)
-    endif ()
-
     # Create imported target FFTW3::fftw3
     add_library(FFTW3::fftw3 STATIC IMPORTED)
     set_target_properties(FFTW3::fftw3 PROPERTIES IMPORTED_LOCATION ${FFTW3_LIBRARY})
-    target_link_libraries(FFTW3::fftw3 INTERFACE ${FFTW3_LIBRARIES})
     target_include_directories(FFTW3::fftw3 INTERFACE ${FFTW3_INCLUDE_DIRS})
+
+    if (UNIX)
+        # Needs `m` on UNIX
+        set(FFTW3_LIBRARIES ${FFTW3_LIBRARIES} m)
+        target_link_libraries(FFTW3::fftw3 INTERFACE m)
+    endif ()
 endif ()
 
